d4b994088ebfa5cbb219aa3de586e89a7dc28c41
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-06-06 08:30:41 +0530

    dom methods

---
+++ June 4 Dom methods/script.js
@@ -0,0 +1,51 @@
+// BOM - browser object model(confirm,prompt,alert)-interaction with the browser window.
+
+
+// let ans=window.confirm("Did you like my website");
+// if(ans){
+//     alert("thank you")
+// }
+// else{
+//     alert("Bhhad me jao")
+// }
+
+
+// reverse the Number
+
+// let num=1234;
+
+// console.log(Number( num.toString().split("").reverse().join("")));
+
+
+// onclick
+
+// let hello=()=>{
+//     alert("hello")
+// }
+
+
+// let fun1=()=>{
+//     alert("i am from function 1")
+// }
+
+// let fun2=()=>{
+//     alert("i am from function 2")
+// }
+
+// dom methods9
+
+let hello=()=>{
+    // document.getElementById("id name")
+    // document.getElementsByClassName("class name")
+    // document.querySelector("#id or .class")
+
+    let ptag =document.querySelector("#text")
+    ptag.innerHTML="<h1> hello world </h1>";
+
+    // ptag.innerTEXT
+    // ptag.textContent
+
+
+}
+
+
--- May 31 loops/script.js
+++ May 31 loops/script.js
@@ -29,11 +29,13 @@
 
 // // for loop for even numbers
 
-// for(let i=0;i<100;i++)
-// {
-//     if(i%2==0)
-//     console.log(i);
-// }
+for(let i=0;i<100;i++)
+{
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+}
 
 // // for loop for odd numbers
 
