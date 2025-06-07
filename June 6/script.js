a8117403098615b6e13d4b6ae683320c9dc099cb
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-06-07 07:55:18 +0530

    event listners

---
+++ June 6/script.js
@@ -0,0 +1,32 @@
+// let hello=()=>{
+//     // alert("Hi i am tushar")
+
+//    let ptag=document.querySelector("#text");
+// //    ptag.innerHTML= "<i> Helloo worldddd </i>";x
+
+// //    CSS using dom
+// //    ptag.style.color="red";
+// //    ptag.style.backgroundColor="yellow";
+
+//    let div=document.querySelector("#color");
+//    div.style.backgroundColor="yellow";
+
+// }
+
+// let hello1=()=>{
+
+//    let ptag=document.querySelector("#text");
+//    let div=document.querySelector("#color");
+//    div.style.backgroundColor="red";
+
+
+// }
+
+let hello3=()=>{
+
+   let ptag=document.querySelector("#text");
+   let div=document.querySelector("#color");
+   div.style.backgroundImage="url(img1.png)";
+
+
+}
