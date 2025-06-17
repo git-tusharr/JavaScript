aaee07f9fd8e8b6f89c077116961c0891fd1a800
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-06-17 07:14:28 +0530

    maos and filter

---
+++ June 16/script.js
@@ -0,0 +1,65 @@
+// .map -iterates every elements perform on element and return an new array
+
+// callback function- jo function kisi functions ke arguments me pass kiya gya ho
+
+// highOrder function- example-.map (jiske arguments me ek function pass hota hai)
+
+
+// let num=[1,2,3,4,5,6,8]
+
+// let newnum=num.map( (e)=>{
+//     return e*2;
+// })
+
+
+
+// let newnum=num.map((e)=>e*2)
+
+// console.log(newnum);
+
+
+let Number=[1,2,3,4,5,6];
+
+let newNumber=Number.map( (e)=>{
+    return e*5;
+})
+console.log(newNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Filter - iterate every element and returns a array after applying filter
+
+
+// let newfilter= num.filter((e)=>{
+//      return e%2==0;
+//     })
+
+//     console.log(newfilter);
+
+
+
+    // for each
+
+
+    // num.forEach((e)=>{
+    //     console.log(e*2);
+    // })
